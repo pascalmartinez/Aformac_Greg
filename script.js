b37e71f9d24c764ba2c6b0ca520e0ebc10f7f21f
Pascal <pmartinez31140@gmail.com>
2018-02-26 11:59:36 +0100

    push initial

---
+++ script.js
@@ -0,0 +1,16 @@
+function news() {
+	// Refus au clic si vide 
+
+	if (document.getElementById('inp').value == "" || document.getElementById('checkb').value==false){
+
+		alert("Veuillez remplir tous les champs s'il vous plait");
+	}
+
+		else {
+			alert('Merci! Vous recevrez bientot vos NewsLetters');
+
+		}
+
+
+}
+
